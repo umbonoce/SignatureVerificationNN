e38ba882668fea40a1c9f774194ac180830abf1d
Umberto Nocerino <umberto.nocerino@edu.sideagroup.com>
2021-04-18 18:30:26 +0200

    Feature extraction

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import numpy as np
+import math
 import os
 import pandas as pd
 from hmmlearn import hmm
-
 import matplotlib.pyplot as plt
+
+
+np.random.seed(42)  # pseudorandomic
 
 
 # tanh normalization; values between 0 and 1
@@ -11,6 +14,21 @@ def normalization(data):
     mean = data.mean()
     std = data.std() + np.finfo(np.float32).eps
     return 0.5 * (np.tanh(0.01 * ((data - mean) / std)) + 1)
+
+
+# time derivative approsimation
+def second_order_regression(data_list):
+    pad_list = data_list.copy()
+    pad_list.append(data_list[len(data_list)-1])
+    pad_list.append(data_list[len(data_list)-1])
+    pad_list.insert(0, data_list[0])
+    pad_list.insert(0, data_list[0])
+    derivata = [None] * (len(data_list))
+
+    for n in range(2, len(data_list)+2):
+        derivata[n-2] = ((pad_list[n+1]-pad_list[n-1]+2*(pad_list[n+2]-pad_list[n-2]))/10)
+
+    return derivata
 
 
 def load_dataset(users):
@@ -24,13 +42,13 @@ def load_dataset(users):
         for file in files:
             if 'ss' in file:
                 df_dict[user]['skilled'].append(pd.read_csv(path + "/" + file, header=0, skiprows=1, sep=' ',
-                                                            names=['X', 'Y', 'TIMESTAMP', 'PENISUP', 'AZIMUTH',
+                                                            names=['X', 'Y', 'TIMESTAMP', 'PENSUP', 'AZIMUTH',
                                                                    'ALTITUDE',
                                                                    'Z']))
 
             elif 'sg' in file:
                 df_dict[user]['genuine'].append(pd.read_csv(path + "/" + file, header=0, skiprows=1, sep=' ',
-                                                            names=['X', 'Y', 'TIMESTAMP', 'PENISUP', 'AZIMUTH',
+                                                            names=['X', 'Y', 'TIMESTAMP', 'PENSUP', 'AZIMUTH',
                                                                    'ALTITUDE', 'Z']))
             else:
                 print(file)
@@ -49,6 +67,7 @@ def initialize_dataset(sign_dict):
             df['Y'] = normalization(df['Y'])
             df['Z'] = normalization(df['Z'])
             compute_features(df)
+            print(df)
         for df in sign_dict[user]['skilled']:
             del df['TIMESTAMP']
             del df['AZIMUTH']
@@ -57,76 +76,104 @@ def initialize_dataset(sign_dict):
             df['Y'] = normalization(df['Y'])
             df['Z'] = normalization(df['Z'])
             compute_features(df)
+            print(df)
     return
 
 
 def compute_features(df):
-    new_df = df.copy()
     e = np.finfo(np.float32).eps
     leng = len(df)
 
-    ''' derivate feature '''
-    dv_list = np.array([])
-    da_list = np.array([])
-    dp_list = np.array([])
-    dtheta_list = np.array([])
-
     x_list = df['X'].tolist()
     y_list = df['Y'].tolist()
-    z_list = df['Z'].tolist()  # pressione
+    z_list = df['Z'].tolist()  # pression
+    up_list = df['PENSUP'].tolist()
 
     dx_list = second_order_regression(x_list)
     dy_list = second_order_regression(y_list)
 
     theta_list = [None] * leng
+    v_list = [None] * leng  # velocity
 
     for i in range(0, leng):
         theta_list[i] = np.arctan(dy_list[i]/(dx_list[i]+e))
+        v_list[i] = np.sqrt(dy_list[i]**2 + dx_list[i]**2)
 
-    print(theta_list)
+    dtheta_list = second_order_regression(theta_list)
+    p_list = [None] * leng  # ro
+    dv_list = second_order_regression(v_list)
+    a_list = [None] * leng  # acceleration
+
+    for i in range(0, leng):
+        dtheta = np.abs(dtheta_list[i] + e)
+        v = np.abs(v_list[i] + e)
+        p_list[i] = math.log(v/dtheta)
+        a_list[i] = np.sqrt((dtheta_list[i]*v_list[i])**2 + dv_list[i]**2)
 
     dz_list = second_order_regression(z_list)
+    da_list = second_order_regression(a_list)
+    dp_list = second_order_regression(p_list)
 
-    v_list = np.array([])  # velocità
-    p_list = np.array([])  # ro
-    a_list = np.array([])  # accelerazione
+    ddx_list = second_order_regression(dx_list)
+    ddy_list = second_order_regression(dy_list)
+
+    ratios_list = [None] * leng  # np.array([])  Ratio of the minimum over the maximum speed over a 5-samples window
+
+    for n in range(0, leng-4):
+        vmin = np.amin(v_list[n:n + 4])
+        vmax = np.amax(v_list[n:n + 4]) + e
+        ratios_list[n] = vmin / vmax
+
+    alphas_list = [None] * leng  # Angle of consecutive samples
+
+    for n in range(0, leng-1):
+        alphas_list[n] = np.arctan((y_list[n + 1] - y_list[n]) / (x_list[n + 1] - x_list[n] + e))
+
+    alphas_list[leng-1] = alphas_list[leng-2]
+
+    dalpha_list = second_order_regression(alphas_list)
+    sinalpha_list = np.array(np.sin(alphas_list))
+    cosalpha_list = np.array(np.cos(alphas_list))
+
+    strokeratio5_list = [None] * leng  # Stroke length to width ratio over a 5-samples window
+
+    for n in range(0, leng-4):
+        stroke_len = np.sum(up_list[n:n+4])
+        width = np.max(x_list[n:n+4]) - np.min(x_list[n:n+4]) + e
+        strokeratio5_list[n] = stroke_len / width
+
+    strokeratio7_list = [None] * leng  # Stroke length to width ratio over a 7-samples window
+
+    for n in range(0, leng-6):
+        stroke_len = np.sum(up_list[n:n+6])
+        width = np.max(x_list[n:n+6]) - np.min(x_list[n:n+6]) + e
+        strokeratio7_list[n] = stroke_len / width
+
+    del df['PENSUP']
+    df['theta'] = theta_list
+    df['v'] = v_list
+    df['ro'] = p_list
+    df['ac'] = a_list
+    df['dX'] = dx_list
+    df['dY'] = dy_list
+    df['dZ'] = dz_list
+    df['dTheta'] = dtheta_list
+    df['dv'] = dv_list
+    df['dRo'] = dp_list
+    df['dAc'] = da_list
+    df['ddX'] = ddx_list
+    df['ddY'] = ddy_list
+    df['vRatio'] = ratios_list
+    df['alpha'] = alphas_list
+    df['dAlpha'] = dalpha_list
+    df['SIN'] = sinalpha_list
+    df['COS'] = cosalpha_list
+    df['5_WIN'] = strokeratio5_list
+    df['7_WIN'] = strokeratio7_list
+
+    return
 
 
-
-
-
-    ddx_list = np.array([])
-    ddy_list = np.array([])
-
-    ratios_list = np.array([])  # Ratio of the minimum over the maximum speed over a 5-samples window
-    alphacons_list = np.array([])  # Angle of consecutive samples
-    dalpha_list = np.array([])
-    sinalpha_list = np.array([])
-    cosalpha_list = np.array([])
-    strokeratio5_list = np.array([])  # Stroke length to width ratio over a 5-samples window
-    strokeratio7_list = np.array([])  # Stroke length to width ratio over a 7-samples window
-
-
-
-    return new_df.copy()
-
-
-def second_order_regression(data_list):
-    pad_list = data_list.copy()
-    pad_list.append(data_list[len(data_list)-1])
-    pad_list.append(data_list[len(data_list)-1])
-    pad_list.insert(0, data_list[0])
-    pad_list.insert(0, data_list[0])
-    derivata = [None] * (len(data_list))
-
-    for n in range(2, len(data_list)+2):
-        derivata[n-2] = ((pad_list[n+1]-pad_list[n-1]+2*(pad_list[n+2]-pad_list[n-2]))/10)
-
-    return derivata
-
-np.random.seed(42)
-
-users = os.listdir('./xLongSignDB')
-df_dict = load_dataset(users)
-
+user_folders = os.listdir('./xLongSignDB')
+df_dict = load_dataset(user_folders)
 initialize_dataset(df_dict)
